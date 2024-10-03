0cf0cedee18e354b90b3d599bab94d7bc360251a
carlosmorais01 <carlos.morais@discente.ufg.br>
2024-10-03 03:22:48 -0300

    movendo try/except e evitando criação de conexão com banco a cada chamada

--- app/crud.py
+++ app/crud.py
@@ -1,11 +1,31 @@
-from flask import request, jsonify, current_app
+from flask import request, jsonify
 from .database import criarConexao
 
-def criarPaciente():
-    tabela = current_app.config.get("TABELA", "pacientes")
-    data = request.get_json()
+_conn = None
+
+def get_db_connection():
+    global _conn
+    if _conn is None:
+        _conn = criarConexao()
+    return _conn
+
+def execute_db_operation(operation):
     try:
-        conn = criarConexao()
+        conn = get_db_connection()
+        result = operation(conn)
+        conn.commit()
+        return result
+    except Exception as e:
+        return jsonify({"erro": f"Erro ao realizar operação no banco de dados: {str(e)}"}), 500
+    finally:
+        conn.close()
+        global _conn
+        _conn = None
+
+def criarPaciente(tabela):
+    data = request.get_json()
+
+    def insertPaciente(conn):
         cur = conn.cursor()
         cur.execute(f"""
             INSERT INTO {tabela} (nome, cpf, data_nascimento, nome_mae, sexo, cartao_sus, 
@@ -13,43 +33,36 @@ def criarPaciente():
             tabagista, etilista, nivel_prioridade, possui_lesao) VALUES (%s, %s, %s, %s, %s, 
             %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (data['nome'], data['cpf'], data['data_nascimento'], data['nome_mae'], data['sexo'], 
-              data['cartao_sus'], data['telefone1'], data['telefone2'], data['email'], 
-              data['cep'], data['bairro'], data['logradouro'], data['complemento'], 
-              data['num_casa'], data['tabagista'], data['etilista'], 
-              data['nivel_prioridade'], data['possui_lesao']))
-        conn.commit()
+                data['cartao_sus'], data['telefone1'], data['telefone2'], data['email'], 
+                data['cep'], data['bairro'], data['logradouro'], data['complemento'], 
+                data['num_casa'], data['tabagista'], data['etilista'], 
+                data['nivel_prioridade'], data['possui_lesao']))
         cur.close()
-        conn.close()
         return jsonify({"mensagem": "Paciente criado"}), 200
-    except Exception as e:
-        return jsonify({"erro": "Erro ao criar o paciente: " + str(e)}), 404
     
-def listarPacientes(tabela="pacientes"):
-    tabela = current_app.config.get("TABELA", "pacientes")
-    try:
-        conn = criarConexao()
+    return execute_db_operation(insertPaciente)
+    
+def listarPacientes(tabela):
+    def getPacientes(conn):
         cur = conn.cursor()
         cur.execute(f"SELECT * FROM {tabela}")
         pacientes = cur.fetchall()
         cur.close()
-        conn.close()
+        
         if not pacientes:
-            return jsonify({"mensagem": "Nenhum paciente encontrado."}), 403
+            return jsonify({"mensagem": "Nenhum paciente encontrado."}), 404
         return jsonify(pacientes), 200
     
-    except Exception as e:
-        return jsonify({"erro": "Erro ao buscar os pacientes."}), 405
+    return execute_db_operation(getPacientes)
     
-def editarPaciente(id, tabela="pacientes"):
-    tabela = current_app.config.get("TABELA", "pacientes")
+def editarPaciente(id, tabela):
     data = request.get_json()
 
-    try:
-        conn = criarConexao()
+    def putPaciente(conn):
         cur = conn.cursor()
-
         cur.execute(f"SELECT * FROM {tabela} WHERE id = %s", (id,))
         paciente = cur.fetchone()
+
         if paciente:
             cur.execute(f"""
                 UPDATE {tabela} 
@@ -64,43 +77,35 @@ def editarPaciente(id, tabela="pacientes"):
                 data.get('complemento'), data['num_casa'], 
                 data['tabagista'], data['etilista'], 
                 data['nivel_prioridade'], data['possui_lesao'], id))
-            conn.commit()
             cur.close()
-            conn.close()
         else:
             return jsonify({"mensagem": "Nenhum paciente encontrado com o ID informado"}), 404
+        
         return jsonify({"mensagem": "Paciente atualizado com sucesso"}), 200
-    except Exception as e:
-        return jsonify({"erro": "Erro ao atualizar o paciente: " + str(e)}), 500
     
-def deletarPaciente(id, tabela="pacientes"):
-    tabela = current_app.config.get("TABELA", "pacientes")
-    try:
-        conn = criarConexao()
+    return execute_db_operation(putPaciente)
+    
+def deletarPaciente(id, tabela):
+    def deletePaciente(conn):
         cur = conn.cursor()
         cur.execute(f"DELETE FROM {tabela} WHERE id = %s", (id,))
         if cur.rowcount == 0:
             return jsonify({"erro": "Paciente não encontrado"}), 404
-        
-        conn.commit()
+
         cur.close()
-        conn.close()
         return jsonify({"mensagem": "Paciente deletado com sucesso"}), 200
-    except Exception as e:
-        return jsonify({"erro": "Erro ao deletar o paciente: " + str(e)}), 406
     
-def buscarPorId(id, tabela="pacientes"):
-    tabela = current_app.config.get("TABELA", "pacientes")
-    try:
-        conn = criarConexao()
+    return execute_db_operation(deletePaciente)
+    
+def buscarPorId(id, tabela):
+    def get_by_id(conn):
         cur = conn.cursor()
         cur.execute(f"SELECT * FROM {tabela} WHERE id = %s", (id,))
         paciente = cur.fetchone()
-        conn.commit()
         cur.close()
-        conn.close()
         if not paciente:
             return jsonify({"mensagem": "Nenhum paciente encontrado."}), 404
+        
         return jsonify(paciente), 200
-    except Exception as e:
-        return jsonify({"erro": "Erro ao paciente usuario: " + str(e)}), 500
+    
+    return execute_db_operation(get_by_id)
--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,26 @@
 from .crud import criarPaciente, listarPacientes, buscarPorId, editarPaciente, deletarPaciente
+from flask import current_app
 
 def setup_routes(app):
-    
+    with app.app_context():
+        tabela = current_app.config.get("TABELA", "pacientes")
+
     @app.route('/pacientes', methods=['POST'])
     def cadastrarPaciente():
-        return criarPaciente()
+        return criarPaciente(tabela)
 
     @app.route('/pacientes', methods=['GET'])
     def verPacientes():
-        return listarPacientes()
+        return listarPacientes(tabela)
     
     @app.route('/pacientes/<int:id>', methods=['GET'])
     def verPaciente(id):
-        return buscarPorId(id)
+        return buscarPorId(id, tabela)
     
     @app.route('/pacientes/<int:id>', methods=['PUT'])
     def editarDados(id):
-        return editarPaciente(id)
+        return editarPaciente(id, tabela)
 
     @app.route('/pacientes/<int:id>', methods=['DELETE'])
     def deletarPessoa(id):
-        return deletarPaciente(id)
+        return deletarPaciente(id, tabela)
